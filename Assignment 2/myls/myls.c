bc9c20919f468028f12dfb29cd0e17e3e4a42982
Tuhil <63782260+Tuhil926@users.noreply.github.com>
2023-04-16 23:54:43 +0530

    Update myls.c

--- Assignment 2/myls/myls.c
+++ Assignment 2/myls/myls.c
@@ -4,6 +4,9 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <pwd.h>
+#include <grp.h>
 // options to include: -l -a -F -S -r
 
 struct ls_opt{
@@ -12,6 +15,14 @@ struct ls_opt{
 	bool F;
 	bool S;
 	bool r;
+};
+
+struct file{
+	struct stat stats;
+	char filename[64];
+	char username[32];
+	char groupname[32];
+	char size[32];
 };
 
 int parse_opt(char* options, struct ls_opt * opts){
@@ -66,26 +77,87 @@ int parse_opt(char* options, struct ls_opt * opts){
 	return 0;
 }
 
-void print_stats(struct stat* file, struct ls_opt* opts, char* filename){
-	if (opts->a || (filename[0] != '.')){
-		printf("%s", filename);
-		if (opts->F){
-			if (S_ISDIR(file->st_mode)){
-				printf("/");
-			}else if(S_IXUSR & file->st_mode){
-				printf("*");
+void get_permissions_string(struct file* file, char* string){
+	if (S_ISDIR(file->stats.st_mode)){
+		string[0] = 'd';
+	}else if (S_ISLNK(file->stats.st_mode)){
+		string[0] = 'l';
+	}
+
+	if (S_IRUSR & file->stats.st_mode){
+		string[1] = 'r';
+	}
+	if (S_IWUSR & file->stats.st_mode){
+		string[2] = 'w';
+	}
+	if (S_IXUSR & file->stats.st_mode){
+		string[3] = 'x';
+	}
+
+	if (S_IRGRP & file->stats.st_mode){
+		string[4] = 'r';
+	}
+	if (S_IWGRP & file->stats.st_mode){
+		string[5] = 'w';
+	}
+	if (S_IXGRP & file->stats.st_mode){
+		string[6] = 'x';
+	}
+
+	if (S_IROTH & file->stats.st_mode){
+		string[7] = 'r';
+	}
+	if (S_IWOTH & file->stats.st_mode){
+		string[8] = 'w';
+	}
+	if (S_IXOTH & file->stats.st_mode){
+		string[9] = 'x';
+	}
+
+}
+
+void print_time(time_t time){
+	char* full_time = ctime(&time);
+	full_time[16] = '\0';
+	printf("%s ", full_time + 4);
+}
+
+void print_stats(struct file* file, struct ls_opt* opts, int max_usrname_len, int max_grpname_len, int max_size_strlen){
+	if (opts->a || (file->filename[0] != '.')){
+		if (opts->l){
+			char perm_string[10] = "----------";
+			get_permissions_string(file, perm_string);
+			printf("%s ", perm_string);
+			printf("%ld ", file->stats.st_nlink);
+			for (int i = 0; i < (-strlen(file->username) + max_usrname_len); i++) {printf(" ");}
+			printf("%s ", file->username);
+			for (int i = 0; i < (-strlen(file->groupname) + max_grpname_len); i++) {printf(" ");}
+				printf("%s ", file->groupname);
+			for (int i = 0; i < (max_size_strlen-strlen(file->size)); i++) {printf(" ");}
+			printf("%s ", file->size);
+			print_time(file->stats.st_mtime);
+			printf("%s", file->filename);
+		}
+		else{
+			printf("%s", file->filename);
+			if (opts->F){
+				if (S_ISDIR(file->stats.st_mode)){
+					printf("/");
+				}else if(S_IXUSR & file->stats.st_mode){
+					printf("*");
+				}
 			}
 		}
 		printf("\n");
 	}
 }
 
-bool compare_stats(struct stat files[256], char* filenames[256], struct ls_opt* opts, int i, int j){
+bool compare_stats(struct file files[256], struct ls_opt* opts, int i, int j){
 	bool res;
 	if (opts->S){
-		res = (files[i].st_size < files[j].st_size);
+		res = (files[i].stats.st_size < files[j].stats.st_size);
 	}else{
-		res = (strcmp(filenames[i], filenames[j]) > 0);
+		res = (strcmp(files[i].filename, files[j].filename) > 0);
 	}
 	if (opts->r){
 		res = !res;
@@ -93,20 +165,16 @@ bool compare_stats(struct stat files[256], char* filenames[256], struct ls_opt* 
 	return res;
 }
 
-void sort_stats(struct stat files[256], int number_of_files, char* filenames[256], struct ls_opt* opts){
-	struct stat temp_file;
-	char* temp_name;
+void sort_stats(struct file files[256], int number_of_files, struct ls_opt* opts){
+	struct file temp_file;
 	for (int j = 0; j < number_of_files; j++){
 		for (int i = 0; i < number_of_files - 1; i++){
-			if (compare_stats(files, filenames, opts, i, i + 1)){
+			if (compare_stats(files, opts, i, i + 1)){
 				temp_file = files[i];
-				temp_name = filenames[i];
 
 				files[i] = files[i + 1];
-				filenames[i] = filenames[i + 1];
 
 				files[i + 1] = temp_file;
-				filenames[i + 1] = temp_name;
 			}
 		}
 	}
@@ -116,20 +184,38 @@ int myls(char* options){
 	DIR *d;
 	struct dirent *dir;
 	d = opendir(".");
-	struct stat files[256];
+	struct file files[256];
 	char* filenames[256];
 	if (!d){
 		printf("error opening current directory.\n");
 		return 1;
 	}
 	int number_of_files = 0;
+
+	int max_usrname_len = 0;
+	int max_grpname_len = 0;
+	int max_size_strlen = 0;
+
+
 	while ((dir = readdir(d)) != NULL){
 		struct stat stat_buf;
 		stat(dir->d_name, &stat_buf);
-		files[number_of_files] = stat_buf;
-		char* filename = (char*)malloc(32*sizeof(char));
-		strcpy(filename, dir->d_name);
-		filenames[number_of_files] = filename;
+		files[number_of_files].stats = stat_buf;
+
+		strcpy((files[number_of_files].username), getpwuid(stat_buf.st_uid)->pw_name);
+		strcpy((files[number_of_files].groupname), getgrgid(stat_buf.st_gid)->gr_name);
+		sprintf((files[number_of_files].size), "%ld", stat_buf.st_size);
+		if (strlen(files[number_of_files].username) > max_usrname_len){
+			max_usrname_len = strlen(files[number_of_files].username);
+		}
+		if (strlen(files[number_of_files].groupname) > max_grpname_len){
+			max_grpname_len = strlen(files[number_of_files].groupname);
+		}
+		if (strlen(files[number_of_files].size) > max_size_strlen){
+			max_size_strlen = strlen(files[number_of_files].size);
+		}
+
+		strcpy(files[number_of_files].filename, dir->d_name);
 		number_of_files++;
 	}
 	closedir(d);
@@ -139,10 +225,10 @@ int myls(char* options){
 		return 1;
 	}
 
-	sort_stats(files, number_of_files, filenames, &opts);
+	sort_stats(files, number_of_files, &opts);
 
 	for (int i = 0; i < number_of_files; i++){
-		print_stats(&files[i], &opts, filenames[i]);
+		print_stats(&files[i], &opts, max_usrname_len, max_grpname_len, max_size_strlen);
 	}
 	
 	return 0;
