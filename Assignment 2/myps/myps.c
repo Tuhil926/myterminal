e1662b8807cda555eb5fcc8a813fc5f4be730348
Tuhil <63782260+Tuhil926@users.noreply.github.com>
2023-04-19 21:25:51 +0530

    Update myps.c

--- Assignment 2/myps/myps.c
+++ Assignment 2/myps/myps.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <dirent.h>
-#include <sys/stat.h>
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
 #include <pwd.h>
-#include <grp.h>
+#include <time.h>
 // options to include: -e -a -f -F
 
 struct ps_opt{
@@ -19,12 +17,24 @@ struct ps_opt{
 struct process{
 	char process_name[64];
 	int pid;
-	//char username[32];
-	//char groupname[32];
-	//char size[32];
+	int ppid;
+	char UID[64];
+	int C;
+	long int size;
+	long int RSS;
+	int PSR;
+	char STIME[10];
+	char TTY[10];
+	char time[8];
 };
 
-int parse_opt(char* options, struct ps_opt * opts){
+void get_time_str(time_t time, char* str){
+	char* full_time = ctime(&time);
+	full_time[16] = '\0';
+	strcpy(str, full_time + 11);
+}
+
+int parse_ps_opt(char* options, struct ps_opt * opts){
 	opts->e = false;
 	opts->a = false;
 	opts->F = false;
@@ -74,43 +84,83 @@ int parse_opt(char* options, struct ps_opt * opts){
 }
 
 void print_processes(struct process* process, struct ps_opt* opts){
-	printf("%d\n", process->pid);
+	printf("%s %d %d %ld %ld %d %s %s\n", process->UID, process->pid, process->ppid, process->size, process->RSS, process->PSR, process->STIME, process->process_name);
 }
 
 int myps(char* options){
 	DIR *d;
 	struct dirent *dir;
 	d = opendir("/proc");
-	struct process processes[256];
+	struct process processes[512];
 	if (!d){
 		printf("error opening current directory.\n");
 		return 1;
 	}
 	int n = 0; // number of processes
 
+	char stats_path[32];
+	uid_t uid;
+	long int stime;
 
 	while ((dir = readdir(d)) != NULL){
-		FILE* process = fopen(dir->d_name, "r");
+		if (dir->d_name[0] == '.' || dir->d_name[0] < 48 || dir->d_name[0] > 57){
+			continue;
+		}
+		stats_path[0] = '\0';
+		strcpy(stats_path, "/proc/");
+		strcpy(stats_path + strlen(stats_path), dir->d_name);
+		strcpy(stats_path + strlen(stats_path), "/stat");
+		FILE* process = fopen(stats_path, "r");
+		if (!process){
+			//printf("Not possible to open\n");
+			continue;
+		}
+		//printf("opening file %s\n", stats_path);
 		for (int i = 0; i < 52; i++){
 			if (i == 0){
-				fscanf("%d", &(process.pid));
-			}else{
-				fscanf("%*s");
+				fscanf(process, "%d ", &(processes[n].pid));
+			}else if (i == 1){
+				fgetc(process);
+				fscanf(process, "%s ", processes[n].process_name);
+				processes[n].process_name[strlen(processes[n].process_name) - 1] = '\0';
+			}else if (i == 3){
+				fscanf(process, "%d ", &(processes[n].ppid));
+			}else if (i == 23){
+				fscanf(process, "%ld ", &(processes[n].RSS));
+			}else if (i == 38){
+				fscanf(process, "%d ", &(processes[n].PSR));
+			}else if (i == 22){
+				fscanf(process, "%ld ", &(processes[n].size));
+			}else if (i == 14){
+				//printf("hsdjf\n");
+				fscanf(process, "%ld ", &stime);
+				get_time_str(stime, processes[n].STIME);
+				//printf("bhifdd\n");
+			}
+			else{
+				fscanf(process, "%*s ");
 			}
 		}
 
+		fclose(process);
+
+		//printf("%s\n", stats_path);
+		printf("bhifdd\n");
+		strcpy(processes[n].UID, "why tf doesn't this work");//getpwuid(processes[n].pid)->pw_name);
+		printf("works!\n");
 		n++;
 	}
 	closedir(d);
 
 	struct ps_opt opts;
-	if (parse_opt(options, &opts)){
+	if (parse_ps_opt(options, &opts)){
 		return 1;
 	}
 
-	for (int i = 0; i < number_of_files; i++){
-		print_stats(&files[i], &opts);
+	for (int i = 0; i < n; i++){
+		print_processes(&processes[i], &opts);
 	}
+	printf("number of processes: %d\n", n);
 	
 	return 0;
 }
