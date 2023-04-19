cf67e4c6a55c9cdbf23a05f2078bf85511e7949e
Tuhil <63782260+Tuhil926@users.noreply.github.com>
2023-04-19 22:39:57 +0530

    Update custom_commands.c

--- Assignment 2/custom_commands.c
+++ Assignment 2/custom_commands.c
@@ -1,5 +1,7 @@
 #include <string.h>
 #include "myls/myls.h"
+#include "myps/myps.h"
+#include "mycp/mycp.h"
 
 int custom_commands(char* command){
 	char *options;
@@ -8,6 +10,16 @@ int custom_commands(char* command){
 		if (command[0] == 'm' && command[1] == 'y' && command[2] == 'l' && command[3] == 's'){
 			options = command + 5;
 			myls(options);
+			return 0;
+		}
+		if (command[0] == 'm' && command[1] == 'y' && command[2] == 'p' && command[3] == 's'){
+			options = command + 5;
+			myps(options);
+			return 0;
+		}
+		if (command[0] == 'm' && command[1] == 'y' && command[2] == 'c' && command[3] == 'p'){
+			options = command + 5;
+			mycp(options);
 			return 0;
 		}
 	}
