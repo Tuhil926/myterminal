2370fe0ae40baaf2d2b4e69352a02baa8c31c7d9
Tuhil <63782260+Tuhil926@users.noreply.github.com>
2023-04-21 00:27:07 +0530

    Update custom_commands.c

--- Assignment 2/custom_commands.c
+++ Assignment 2/custom_commands.c
@@ -2,6 +2,7 @@
 #include "myls/myls.h"
 #include "myps/myps.h"
 #include "mycp/mycp.h"
+#include "mygrep/mygrep.h"
 
 int custom_commands(char* command){
 	char *options;
@@ -19,7 +20,18 @@ int custom_commands(char* command){
 		}
 		if (command[0] == 'm' && command[1] == 'y' && command[2] == 'c' && command[3] == 'p'){
 			options = command + 5;
-			mycp(options);
+			mycp(options, 0);
+			return 0;
+		}
+		if (command[0] == 'm' && command[1] == 'y' && command[2] == 'm' && command[3] == 'v'){
+			options = command + 5;
+			mycp(options, 1);
+			return 0;
+		}
+	}else if (length == 6 || length > 6 && command[6] == ' '){
+		if (command[0] == 'm' && command[1] == 'y' && command[2] == 'g' && command[3] == 'r' && command[4] == 'e' && command[5] == 'p'){
+			options = command + 7;
+			mygrep(options);
 			return 0;
 		}
 	}
