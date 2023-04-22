21c9e5eb74faacc7eb142ab97f58a8c4e9072885
Tuhil <63782260+Tuhil926@users.noreply.github.com>
2023-04-22 22:58:03 +0530

    Update myps.c

--- Assignment 2/myps/myps.c
+++ Assignment 2/myps/myps.c
@@ -5,63 +5,38 @@
 #include <stdlib.h>
 #include <pwd.h>
 #include <time.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/sysinfo.h>
-// options to include: -e -a -f -F
+#include <sys/stat.h>
+// options to include: -f -i -n -v
 
-struct ps_opt{
-	bool e;
-	bool a;
-	bool F;
+struct cp_opt{
 	bool f;
+	bool i;
+	bool n;
+	bool v;
+	char* filenames[16];
+	int number_of_files;
 };
 
 struct process{
 	char process_name[64];
 	int pid;
 	int ppid;
-	int UID;
-	char username[64];
+	char UID[64];
 	int C;
 	long int size;
 	long int RSS;
 	int PSR;
-	char STIME[32];
-	long int STIME_int;
-	int TTY_num;
+	char STIME[10];
 	char TTY[10];
-	long int TIME;
-	char time_str[10];
+	char time[8];
 };
 
-void get_time_str(time_t time, char* str){
-	char* full_time = ctime(&time);
-	full_time[16] = '\0';
-	strcpy(str, full_time + 11);
-}
-
-void make_to_hms_format(long int time_in_sec, char* str){
-	int sec = time_in_sec % 60;
-	time_in_sec /= 60;
-	int min = time_in_sec % 60;
-	time_in_sec /= 60;
-	int hrs = time_in_sec % 60;
-	strcpy(str, "00:00:00");
-	str[0] = hrs/10 + 48;
-	str[1] = hrs%10 + 48;
-	str[3] = min/10 + 48;
-	str[4] = min%10 + 48;
-	str[6] = sec/10 + 48;
-	str[7] = sec%10 + 48;
-}
-
-int parse_ps_opt(char* options, struct ps_opt * opts){
-	opts->e = false;
-	opts->a = false;
-	opts->F = false;
+int parse_cp_opt(char* options, struct cp_opt * opts){
 	opts->f = false;
+	opts->i = false;
+	opts->n = false;
+	opts->v = false;
+	opts->number_of_files = 0;
 
 	char* temp = options - 1;
 
@@ -73,6 +48,7 @@ int parse_ps_opt(char* options, struct ps_opt * opts){
 		if (*temp == ' ' && *(temp + 1) != ' '){
 			tokens[number_of_tokens] = temp + 1;
 			number_of_tokens++;
+			*temp = '\0';
 		}
 		temp++;
 	}
@@ -83,19 +59,20 @@ int parse_ps_opt(char* options, struct ps_opt * opts){
 			continue;
 		}
 		if (*character != '-'){
-			printf("invalid argument: %s\n", character);
-			return 1;
+			opts->filenames[opts->number_of_files] = character;
+			opts->number_of_files++;
+			continue;
 		}
 		character++;
 		while (*character != ' ' && *character != '\0'){
-			if (*character == 'e'){
-				opts->e = true;
-			}else if (*character == 'a'){
-				opts->a = true;
-			}else if (*character == 'F'){
-				opts->F = true;
-			}else if (*character == 'f'){
+			if (*character == 'f'){
 				opts->f = true;
+			}else if (*character == 'i'){
+				opts->i = true;
+			}else if (*character == 'n'){
+				opts->n = true;
+			}else if (*character == 'v'){
+				opts->v = true;
 			}else{
 				printf("Invalid option: %c\n", *character);
 				return 1;
@@ -103,154 +80,244 @@ int parse_ps_opt(char* options, struct ps_opt * opts){
 			character++;
 		}
 	}
+
+	if (opts->number_of_files < 2){
+		printf("Not enough argumants!\n");
+		return 1;
+	}
+
 	return 0;
 }
 
-void print_processes(struct process* process, struct ps_opt* opts){
-	if (opts->F){
-		printf("%20s %6d %6d %3d %10ld %10ld %2d %5s %5s %s %s\n", process->username, process->pid, process->ppid, process->C, process->size, process->RSS, process->PSR, process->STIME, process->TTY, process->time_str, process->process_name);
-	}else if (opts->f){
-		printf("%20s %6d %6d %3d %5s %5s %s\n", process->username, process->pid, process->ppid, process->C, process->STIME, process->TTY, process->process_name);		
-	}else{
-		printf("%6d %5s %s %s\n", process->pid, process->TTY, process->time_str, process->process_name);
-	}
-}
-
-void print_headings(struct ps_opt* opts){
-	if (opts->F){
-		printf("%20s %6s %6s %3s %10s %10s %3s %5s %5s %8s %s\n", "UID", "PID", "PPID", "C", "SIZE", "RSS", "PSR", "STIME", "TTY", "TIME", "CMD");
-	}else if (opts->f){
-		printf("%20s %6s %6s %3s %5s %5s %s\n", "UID", "PID", "PPID", "C", "STIME", "TTY", "CMD");		
-	}else{
-		printf("%6s %5s %8s %s\n", "PID", "TTY", "TIME", "CMD");
-	}
-}
-
-int myps(char* options){
-	DIR *d;
-	struct dirent *dir;
-	d = opendir("/proc");
-	struct process processes[512];
-	if (!d){
-		printf("error opening current directory.\n");
+int mycp(char* options, int should_move){
+	struct cp_opt opts;
+	if (parse_cp_opt(options, &opts)){
 		return 1;
 	}
-	int n = 0; // number of processes
 
-	char stats_path[512];
-	struct sysinfo sys_info;
-	sysinfo(&sys_info);
-	long uptime = sys_info.uptime;
-	long now = time(NULL);
-	long boot_time = now - uptime;
-	uid_t uid;
-	long int stime = 0;
-	long int utime = 1;
+	char buff[4096];
+	char char_buf;
 
-	while ((dir = readdir(d)) != NULL){
-		if (dir->d_name[0] == '.' || dir->d_name[0] < 48 || dir->d_name[0] > 57){
-			continue;
-		}
-		sprintf(stats_path, "/proc/%s/stat", dir->d_name);
-		FILE* process = fopen(stats_path, "r");
-		if (!process){
-			//printf("Not possible to open\n");
-			continue;
-		}
-		//printf("opening file %s\n", stats_path);
-		for (int i = 0; i < 52; i++){
-			if (i == 0){
-				fscanf(process, "%d ", &(processes[n].pid));
-			}else if (i == 1){
-				fgetc(process);
-				fscanf(process, "%s ", processes[n].process_name);
-				processes[n].process_name[strlen(processes[n].process_name) - 1] = '\0';
-			}else if (i == 3){
-				fscanf(process, "%d ", &(processes[n].ppid));
-			}else if (i == 38){
-				fscanf(process, "%d ", &(processes[n].PSR));
-			//}else if (i == 14){
-			// 	//printf("hsdjf\n");
-			// 	fscanf(process, "%ld ", &stime);
-			// 	get_time_str(stime, processes[n].STIME);
-			// 	//printf("bhifdd\n");
-			}else if (i == 13){
-				fscanf(process, "%ld ", &utime);
-				fscanf(process, "%ld ", &stime);
-				//stime /= sysconf(_SC_CLK_TCK);
-				make_to_hms_format((stime + utime)/sysconf(_SC_CLK_TCK), processes[n].time_str);
-				i++;
-			}else if (i == 21){
-				fscanf(process, "%ld ", &(processes[n].STIME_int));
-				processes[n].STIME_int /= sysconf(_SC_CLK_TCK);
-				get_time_str(boot_time + processes[n].STIME_int, processes[n].STIME);
-			}
-			else{
-				fscanf(process, "%*s ");
-			}
-		}
+	char* destination = opts.filenames[opts.number_of_files - 1];
 
-		fclose(process);
+	struct stat stat_buf;
+	struct stat stat_buf_2;
 
-		sprintf(stats_path, "/proc/%s/statm", dir->d_name);
-		process = fopen(stats_path, "r");
-		if (process == NULL){
-			printf("Error opening file\n");
-		}
-
-		fscanf(process, "%ld", &(processes[n].size));
-		fscanf(process, "%ld", &(processes[n].RSS));
-		processes[n].RSS *= 4;
-
-		fclose(process);
-
-		sprintf(stats_path, "/proc/%s/status", dir->d_name);
-		process = fopen(stats_path, "r");
-		if (process == NULL){
-			printf("Error opening file\n");
-		}
-
-		char line[1000];
-		while(fgets(line, 1000, process)){
-			if (sscanf(line, "Uid:\t%d", &(processes[n].UID))){
-				break;
-			}
-		}
-		fclose(process);
-
-		sprintf(stats_path, "/proc/%s/fd/0", dir->d_name);
-		int fd = open(stats_path, 'r');
-		processes[n].TTY_num = fd;
-		char* tty = ttyname(fd);
-		if (tty == NULL){
-			strcpy(processes[n].TTY, "?");	
+	for (int i = 0; i < opts.number_of_files - 1; i++){
+		if (stat(opts.filenames[i], &stat_buf)){
+			printf("File \"%s\" doesn't exist!\n", opts.filenames[i]);
+			return 1;
 		}else{
-			strcpy(processes[n].TTY, ttyname(fd) + 5);
-		}
-		
-
-		strcpy(processes[n].username, getpwuid(processes[n].UID)->pw_name);
-		processes[n].C = processes[n].STIME_int != 0? ((stime + utime)*100/sysconf(_SC_CLK_TCK)/(now - (boot_time + processes[n].STIME_int))):0;
-		n++;
-	}
-	closedir(d);
-
-	struct ps_opt opts;
-	if (parse_ps_opt(options, &opts)){
-		return 1;
-	}
-
-	sprintf(stats_path, "/proc/%d/fd/0", getpid());
-	int fd = open(stats_path, 'r');
-	char* tty = ttyname(fd) + 5;
-
-	print_headings(&opts);
-	for (int i = 0; i < n; i++){
-		if (opts.e || (opts.a && processes[i].TTY[0] != '?') || (strcmp(processes[i].TTY, tty) == 0)){
-			print_processes(&processes[i], &opts);
+			if (S_ISDIR(stat_buf.st_mode)){
+				printf("\"%s\" is a directory! This command cannot copy a directory!\n", opts.filenames[i]);
+				return 1;
+			}
 		}
 	}
-	//printf("number of processes: %d\n", n);
-	
+
+	if (stat(destination, &stat_buf)){
+		if (opts.number_of_files != 2){
+			printf("Error: Directory \"%s\" doesn't exist!\n", destination);
+			return 1;
+		}else{
+			// create new file
+			FILE* newfile = fopen(opts.filenames[1], "wb");
+			FILE* original = fopen(opts.filenames[0], "rb");
+			if (newfile == NULL || original == NULL){
+				printf("Error opening file!\n");
+				return 1;
+			}
+			// while (fgets(buff, 4096, original)){
+			// 	fputs(buff, newfile);
+			// }
+			//printf("lol\n");
+			while (fread(&char_buf, 1, 1, original)){
+				fwrite(&char_buf, 1, 1, newfile);
+			}
+			fflush(newfile);
+			fclose(original);
+			fclose(newfile);
+			stat(opts.filenames[0], &stat_buf_2);
+			chmod(opts.filenames[1], stat_buf_2.st_mode);
+			if (!should_move){
+				if (opts.v){
+					printf("successfully copied file: %s\n", opts.filenames[1]);
+				}
+			}else{
+				remove(opts.filenames[0]);
+				if (opts.v){
+					printf("successfully moved file: %s\n", opts.filenames[1]);
+				}
+			}
+
+		}
+	}else{
+		if (S_ISDIR(stat_buf.st_mode)){
+			if (opts.v){
+				printf("putting files in directory: %s\n", destination);
+			}
+			// put the files in this directory
+			char newfile_path[256];
+			for (int i = 0; i < opts.number_of_files - 1; i++){
+				sprintf(newfile_path, "%s/%s", destination, opts.filenames[i]);
+				int exists = !stat(newfile_path, &stat_buf);
+				if (!exists || opts.f){
+					if (exists && opts.f){
+						remove(newfile_path);
+					}
+					FILE* newfile = fopen(newfile_path, "wb");
+					FILE* original = fopen(opts.filenames[i], "rb");
+					if (newfile == NULL || original == NULL){
+						printf("Error opening file!\n");
+						return 1;
+					}
+					while (fread(&char_buf, 1, 1, original)){
+						fwrite(&char_buf, 1, 1, newfile);
+					}
+					fflush(newfile);
+					fclose(original);
+					fclose(newfile);
+					stat(opts.filenames[i], &stat_buf_2);
+					chmod(newfile_path, stat_buf_2.st_mode);
+					if (!should_move){
+						if (opts.v){
+							printf("successfully copied file: %s to directory: %s\n", opts.filenames[i], destination);
+						}
+					}else{
+						remove(opts.filenames[i]);
+						if (opts.v){
+							printf("successfully moved file: %s to directory: %s\n", opts.filenames[i], destination);
+						}
+					}
+				}else{
+					if (opts.n){
+						if (opts.v){
+							printf("didn't overwrite file: %s\n", newfile_path);
+						}
+					}else if (opts.i){
+						char choice[128];
+						printf("Would you like to overwrite file %s?(y/n): ", newfile_path);
+						fgets(choice, 128, stdin);
+						if (choice[0] == 'y'){
+							FILE* newfile = fopen(newfile_path, "wb");
+							FILE* original = fopen(opts.filenames[i], "rb");
+							if (newfile == NULL || original == NULL){
+								printf("Error opening file!\n");
+								return 1;
+							}
+							while (fread(&char_buf, 1, 1, original)){
+								fwrite(&char_buf, 1, 1, newfile);
+							}
+							fflush(newfile);
+							fclose(original);
+							fclose(newfile);
+							stat(opts.filenames[i], &stat_buf_2);
+							chmod(newfile_path, stat_buf_2.st_mode);
+							if (!should_move){
+								if (opts.v){
+									printf("successfully copied file: %s to directory: %s\n", opts.filenames[i], destination);
+								}
+							}else{
+								remove(opts.filenames[i]);
+								if (opts.v){
+									printf("successfully moved file: %s to directory: %s\n", opts.filenames[i], destination);
+								}
+							}
+						}else if (opts.v){
+							printf("didn't overwrite file: %s\n", newfile_path);
+						}
+					}else{
+						FILE* newfile = fopen(newfile_path, "wb");
+						FILE* original = fopen(opts.filenames[i], "rb");
+						if (newfile == NULL || original == NULL){
+							printf("Error opening file!\n");
+							return 1;
+						}
+						while (fread(&char_buf, 1, 1, original)){
+							fwrite(&char_buf, 1, 1, newfile);
+						}
+						fflush(newfile);
+						fclose(original);
+						fclose(newfile);
+						stat(opts.filenames[i], &stat_buf_2);
+						chmod(newfile_path, stat_buf_2.st_mode);
+						if (!should_move){
+							if (opts.v){
+								printf("successfully copied file: %s to directory: %s\n", opts.filenames[i], destination);
+							}
+						}else{
+							remove(opts.filenames[i]);
+							if (opts.v){
+								printf("successfully moved file: %s to directory: %s\n", opts.filenames[i], destination);
+							}
+						}
+					}
+				}
+			}
+		}else if (opts.number_of_files != 2){
+			printf("Error: Too many files given! cannot copy multiple files to a single file!\n");
+			return 1;
+		}else{
+			if (opts.n){
+				if (opts.v){
+					printf("The file \"%s\" already exists!\n", destination);
+				}
+			}else if (opts.i){
+				// overwrite the file
+				char choice[128];
+				printf("Would you like to overwrite file %s?(y/n): ", destination);
+				fgets(choice, 128, stdin);
+				if (choice[0] == 'y'){
+					FILE* newfile = fopen(opts.filenames[1], "wb");
+					FILE* original = fopen(opts.filenames[0], "rb");
+					if (newfile == NULL || original == NULL){
+						printf("Error opening file!\n");
+						return 1;
+					}
+					while (fread(&char_buf, 1, 1, original)){
+						fwrite(&char_buf, 1, 1, newfile);
+					}
+					fflush(newfile);
+					fclose(original);
+					fclose(newfile);
+					stat(opts.filenames[0], &stat_buf_2);
+					chmod(opts.filenames[1], stat_buf_2.st_mode);
+					if (opts.v){
+						printf("successfully overwrote file: %s to file: %s\n", opts.filenames[0], opts.filenames[1]);
+					}
+					if (should_move){
+						remove(opts.filenames[0]);
+					}
+				}else if (opts.v){
+					printf("didn't overwrite file: %s\n", destination);
+				}
+			}else{
+				if (opts.f){
+					remove(opts.filenames[1]);
+				}
+				FILE* newfile = fopen(opts.filenames[1], "wb");
+				FILE* original = fopen(opts.filenames[0], "rb");
+				if (newfile == NULL || original == NULL){
+					printf("Error opening file!\n");
+					return 1;
+				}
+				while (fread(&char_buf, 1, 1, original)){
+					fwrite(&char_buf, 1, 1, newfile);
+				}
+				fflush(newfile);
+				fclose(original);
+				fclose(newfile);
+				stat(opts.filenames[0], &stat_buf_2);
+				chmod(opts.filenames[1], stat_buf_2.st_mode);
+				if (opts.v){
+					printf("successfully overwrote file: %s to file: %s\n", opts.filenames[0], opts.filenames[1]);
+				}
+				if (should_move){
+					remove(opts.filenames[0]);
+				}
+			}
+		}
+	}
+
 	return 0;
 }
